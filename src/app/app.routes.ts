d1d827bbbd319bda0835623388daad84b352546d
Rodrigo Cevas <rcevas@outlook.com>
2025-05-29 10:04:50 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CarteleraComponent } from './features/cartelera/cartelera.component';
+import { FichaEventoComponent } from './features/ficha-evento/ficha-evento.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CarteleraComponent },
+  { path: 'evento', component: FichaEventoComponent },
+  { path: '**', redirectTo: '' }
+];
