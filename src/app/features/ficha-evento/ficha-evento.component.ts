cf73e09339035ae4f7bf6c7eb8a30cb219946693
Rodrigo Cevas <rcevas@outlook.com>
2025-05-30 15:03:44 +0200

    increase decrease btn

--- src/app/features/ficha-evento/ficha-evento.component.ts
+++ src/app/features/ficha-evento/ficha-evento.component.ts
@@ -3,16 +3,21 @@ import { ActivatedRoute } from '@angular/router';
 import { EVENTS } from '../../core/data/data-events';
 import { EventData } from '../../core/models/event';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ficha-evento',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ficha-evento.component.html',
   styleUrl: './ficha-evento.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FichaEventoComponent {
+  value: number = 0;
+  min: number = 0;
+  max: number = 100;
+
   event = signal<EventData>({
     id: '',
     title: '',
@@ -31,6 +36,17 @@ export class FichaEventoComponent {
     const filteredEvent = EVENTS.find((event) => event.id === eventId);
     if (filteredEvent) {
       this.event.set(filteredEvent);
+    }
+  }
+
+  changeValue(delta: number, index: number): void {
+    const sessionRow = document.querySelector(
+      '#input_' + index
+    ) as HTMLInputElement;
+    console.log(index);
+    const newValue = this.value + delta;
+    if (newValue >= this.min && newValue <= this.max) {
+      this.value = newValue;
     }
   }
 }
