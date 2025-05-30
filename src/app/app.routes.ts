f46bcf7146da767023ff1965b7128bfc5b53a3c5
Rodrigo Cevas <rcevas@outlook.com>
2025-05-30 08:57:53 +0200

    routing path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { CarteleraComponent } from './features/cartelera/cartelera.component';
 import { FichaEventoComponent } from './features/ficha-evento/ficha-evento.component';
 
 export const routes: Routes = [
-  { path: '', component: CarteleraComponent },
+  { path: '', redirectTo: '/cartelera', pathMatch: 'full' },
+  { path: 'cartelera', component: CarteleraComponent },
   { path: 'evento', component: FichaEventoComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '/cartelera', pathMatch: 'full' },
 ];
